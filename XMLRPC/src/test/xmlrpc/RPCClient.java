e5a7585c6d83d62b45df5541bb4155dd6d07b734
praveenjalem@gmail.com <JalemV@Blrpraveenlt.ap.tsacorp.com>
2012-09-20 23:40:20 +0530

    xmlrpc changes

--- XMLRPC/src/test/xmlrpc/RPCClient.java
+++ XMLRPC/src/test/xmlrpc/RPCClient.java
@@ -6,17 +6,20 @@ import java.net.URL;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
-import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
 
 public class RPCClient {
 
 	public static void main(String[] args) throws IOException, XmlRpcException {
 		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-		config.setServerURL(new URL("http://localhost:8080/xmlrpc"));
+		//config.setServerURL(new URL("http://localhost:8080/XMLRPC/xmlrpc"));
+		config.setServerURL(new URL("http://localhost:8080/XMLRPC/xmlrpc2"));
+		config.setBasicPassword("admin");
+		config.setBasicUserName("admin");
+		
 		XmlRpcClient client = new XmlRpcClient();
-		client.setTransportFactory(new XmlRpcCommonsTransportFactory(client));
 		// transport factory determines the way the client communicates with the
 		// server
+		//client.setTransportFactory(new XmlRpcCommonsTransportFactory(client));//This doesn't work due to closure of commons httpclient project.
 		client.setConfig(config);
 		Object[] params = new Object[] { new Integer(33), new Integer(44) };
 		Integer result = (Integer) client.execute("Calculator.addIntegers",
